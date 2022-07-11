474cf24ccbaae3b1b4580b2ce43c9744896ade9a
Ashutosh Pandey <87969744+ashutoshpandeydu@users.noreply.github.com>
2022-07-11 18:24:02 +0530

    Tested logger

--- SwaggerTest/Controllers/WeatherForecastController.cs
+++ SwaggerTest/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace SwaggerTest.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogWarning("This is warning");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
